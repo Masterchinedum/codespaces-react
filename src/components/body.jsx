73bc5deb15b1c227b4cd4b338b17a6b4111bf948
CHINEDU KINGSLEY OKEREKE <Chinedu02k@gmail.com>
2023-12-29 09:22:54 +0000

    Refactor body component to include a form for meme generation

--- src/components/body.jsx
+++ src/components/body.jsx
@@ -1,10 +1,20 @@
 
 const Body = () => {
     return (
-        <div>
-            <h1>Body </h1>
+        <div className="text-field-cont">
+            <form className="text-field">
+                <div className="top-textcont">
+                    <label className="top-text-lab">Top Text</label>
+                    <input type="text" className="top-text" placeholder="Shut up" />
+                </div>
+                <div className="bottom-textcont">
+                    <label className="bottom-text-lab">Bottom text</label>
+                    <input type="text" className="bottom-text" placeholder="And take my money" />
+                </div>
+            </form>
+            <div> <button className="form-buttom" type="submit">Get a new meme image</button> </div> 
         </div>
-    )
-}
+    );
+};
 
 export default Body;
