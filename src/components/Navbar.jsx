bfec5b23deb8d460629115173f7b25054be291ab
CHINEDU KINGSLEY OKEREKE <Chinedu02k@gmail.com>
2023-12-29 02:16:10 +0000

    Add navigation container styling

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav className='navCont'>
+            <div className='logoCont'>
+                <img src="../../public/(Troll Face).png" className= "logo" alt="logo" />
+                <h3 className='logo-text'>Meme Generator</h3>
+            </div>
+            <div className='menuIt'>
+            React Course - Project 3
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
