88d68c479f40c4ddbbd44619faa15753a816f702
CHINEDU KINGSLEY OKEREKE <Chinedu02k@gmail.com>
2023-12-29 09:05:52 +0000

    Add Body component to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import Body from './components/body';
 import './App.css';
 
 const App = () => {
   return (
     <div className='App'>
       <Navbar />
+      <Body />
     </div>
   )
 }
