431452609f586f5199bd43d3660ac757e34fd2fa
CHINEDU KINGSLEY OKEREKE <Chinedu02k@gmail.com>
2024-01-03 05:07:15 +0000

    Update user authentication logic

---
+++ src/projects/mappingarray.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+    const thingsArray = ["Thing 1", "Thing 2"]
+    
+    const display = thingsArray.map(thing => <p> {thing} </p>)
+    /**
+     * Challenge: Map over the thingsArray to generate
+     * a <p> element for each item and render them on the page
+     * below the button
+     * Done
+     */
+    
+    return (
+        <div>
+            <button>Add Item</button>
+            {display}
+        </div>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
