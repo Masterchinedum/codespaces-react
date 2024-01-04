0323e12538569f5902952eebdefe5626016f7837
CHINEDU KINGSLEY OKEREKE <Chinedu02k@gmail.com>
2024-01-04 10:49:35 +0000

    Refactor counter.jsx to increment count on button click

--- src/projects/counter.jsx
+++ src/projects/counter.jsx
@@ -2,22 +2,24 @@ import React from "react"
 
 export default function App() {
     /**
-     * Challenge: Create a function called `add` that runs
-     * when the + button is clicked. (Can just console.log("add") for now)
+     * Challenge: 
+     * See if you can think of a way to add 1 to the count
+     * every time the + button is clicked
      */
     const [count, setCount] = React.useState(0)
     
-    const add = () => { 
-        console.log (`${count + 1}`); 
-        };
+    function add() {
+        setCount(prevCount => prevCount + 1);
+    }
+
     
     return (
         <div className="counter">
-            <button className="counter--minus">–</button>
+            <button className="counter--minus" onClick={remove}>–</button>
             <div className="counter--count">
                 <h1>{count}</h1>
             </div>
-            <button className="counter--plus" onClick= {add}>+</button>
+            <button className="counter--plus" onClick={add}>+</button>
         </div>
     )
 }
