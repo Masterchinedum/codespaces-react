e0c04ba073827c80d4408ff01063b0f03b33de9b
CHINEDU KINGSLEY OKEREKE <Chinedu02k@gmail.com>
2024-01-04 10:24:26 +0000

    Add state and click event listener to update state

--- src/projects/usestate.jsx
+++ src/projects/usestate.jsx
@@ -1,17 +1,24 @@
 import React from "react"
 
 export default function App() {
+    const [isImportant, setIsImportant] = React.useState("Yes")
     /**
-     * Challenge: Replace our hard-coded "Yes" on the page with 
-     * some state initiated with React.useState()
+     * Challenge: 
+     * 1. Create a function called `handleClick` that runs
+     *    setIsImportant("No")
+     * 2. add a click event listener to the div.state--value
+     *    that runs `handleClick` when the div is clicked.
      */
-    const [result , setResult] = React.useState("Hello")
-    console.log(result)
+    
+    function handleClick() {
+        setIsImportant("No")
+    }
+     
     return (
         <div className="state">
             <h1 className="state--title">Is state important to know?</h1>
-            <div className="state--value">
-                <h1>{result}</h1>
+            <div className="state--value" onClick = {handleClick}>
+                <h1>{isImportant}</h1>
             </div>
         </div>
     )
