fc3f3ff6ef67be1a8258fb8fdc4038d8a87bd826
Masterchinedum <chinedu02k@gmail.com>
2024-01-07 14:15:26 +0800

    Update code formatting and fix minor bugs

---
+++ src/projects/memegenUpdated/components/Meme.js
@@ -0,0 +1,60 @@
+import React from "react";
+import memesData from "../memesData.js";
+
+export default function Meme() {
+  const [meme, setMeme] = React.useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "http://i.imgflip.com/1bij.jpg",
+  });
+  const [allMemeImages, setAllMemeImages] = React.useState(memesData);
+
+  function getMemeImage() {
+    const memesArray = allMemeImages.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    const url = memesArray[randomNumber].url;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      randomImage: url,
+    }));
+  }
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      [name]: value,
+    }));
+  };
+
+  return (
+    <main>
+      <div className="form">
+        <input
+          type="text"
+          name="topText"
+          value={meme.topText}
+          placeholder="Top text"
+          className="form--input"
+          onChange={handleInputChange}
+        />
+        <input
+          type="text"
+          name="bottomText"
+          value={meme.bottomText}
+          placeholder="Bottom text"
+          className="form--input"
+          onChange={handleInputChange}
+        />
+        <button className="form--button" onClick={getMemeImage}>
+          Get a new meme image 🖼
+        </button>
+      </div>
+      <div className="meme">
+        <img src={meme.randomImage} className="meme--image" />
+        <h2 className="meme--text top">{meme.topText}</h2>
+        <h2 className="meme--text bottom">{meme.bottomText}</h2>
+      </div>
+    </main>
+  );
+}
